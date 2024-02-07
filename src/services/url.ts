7ccbd582929f8ad1d4177bcac8cab12c4f13b96c
nghyane <hoangvananhnghia@gmail.com>
2024-02-07 22:36:26 +0700

    fix slug

--- src/services/url.ts
+++ src/services/url.ts
@@ -1,21 +1,19 @@
 import slugify from "slugify";
 
 const titleToSlug = (title: string) => {
-    // limit to 100 characters
-    return slugify(title, {
-        locale: 'ja',
-        remove: /[*+～ー?.()'"!:@]/g,
-        lower: true,
-        replacement: '-',
-    }).slice(0, 150);
-}
+  // limit to 100 characters
+  return slugify(title, {
+    locale: "ja",
+    remove: /[*+～ー?.()'"!:@\/]/g,
+    lower: true,
+    replacement: "-",
+  }).slice(0, 150);
+};
 
-export const mangaUrl = ({title, id} : {title: string, id: number}) => {
-    return `/manga/${
-        titleToSlug(title)
-    }/${id}`;
-}
+export const mangaUrl = ({ title, id }: { title: string; id: number }) => {
+  return `/manga/${titleToSlug(title)}/${id}`;
+};
 
-export const chapterUrl = ({id} : {id: number}) => {
-    return `/read?id=${id}`;
-}
+export const chapterUrl = ({ id }: { id: number }) => {
+  return `/read?id=${id}`;
+};
