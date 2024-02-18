20960595139d75849151ec732f5f30670b124d1b
nghyane <hoangvananhnghia@gmail.com>
2024-02-18 20:00:36 +0700

    add tag page

--- src/controllers/TagController.tsx
+++ src/controllers/TagController.tsx
@@ -1,26 +1,37 @@
 import { VerifyContext } from "@/derives/VerifyToken";
 import prisma from "@/services/prisma";
+import TagPage from "@/views/pages/tag";
 
 class TagController {
   async index(ctx: VerifyContext) {
-   const { id } = ctx.params;
+    const { id } = ctx.params;
 
-  if (!id) {
-    throw new Error("User id is required");
-  }
+    if (!id) {
+      throw new Error("User id is required");
+    }
 
-  const mangas = await prisma.manga.findMany({
-    where: {
-      tags: {
-        some: {
-          id: parseInt(id),
+    const tag = await prisma.tag.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    const mangas = await prisma.manga.findMany({
+      where: {
+        tags: {
+          some: {
+            id: parseInt(id),
+          },
         },
       },
-    },
-  });
+      select: {
+        tags: true,
+        title: true,
+        id: true,
+      },
+    });
 
-  console.log(mangas);
-
+    return TagPage({ tag, mangas });
   }
 }
 
---
+++ src/views/pages/tag.tsx
@@ -0,0 +1,68 @@
+import BaseHead from "../components/BaseHead";
+import BaseBody from "../components/BaseBody";
+import HomeFooter from "../components/HomeFooter";
+import { mangaUrl } from "@/services/url";
+
+type TagPageProps = {
+  tag: {
+    name: string;
+  };
+  mangas: {
+    id: number;
+    title: string;
+    cover: string;
+    tags: {
+      id: number;
+      name: string;
+    }[];
+  }[];
+};
+
+const TagPage = (props: TagPageProps) => {
+  const { tag, mangas } = props;
+
+  return (
+    <html>
+      <BaseHead>
+        <title safe>{tag.name}</title>
+        <meta name="description" content={`Mangas with tag ${tag.name}`} />
+      </BaseHead>
+      <BaseBody>
+        <div class="container mt-10">
+          <h1 safe class="text-primary mb-3 text-3xl font-bold">
+            #{tag.name}
+          </h1>
+
+          <ul class="flex list-disc flex-wrap gap-2">
+            {mangas.map((manga) => (
+              <li class="w-full">
+                <a href={mangaUrl(manga)} title={manga.title} safe>
+                  {manga.title}
+                </a>
+
+                <hr class="my-2" />
+                <div class="flex flex-wrap gap-2">
+                  {manga.tags.map((tag) => (
+                    <a
+                      href={`/tag/${tag.id}`}
+                      title={tag.name}
+                      class="bg-primary max-w-full overflow-hidden truncate rounded-md p-1 text-xs text-white"
+                      safe
+                    >
+                      {tag.name}
+                    </a>
+                  ))}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <HomeFooter />
+
+      </BaseBody>
+    </html>
+  );
+};
+
+export default TagPage;
