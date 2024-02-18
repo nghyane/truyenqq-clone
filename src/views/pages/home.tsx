932308ac88f85d155e5b3bdbcc169039cdba81fa
nghyane <hoangvananhnghia@gmail.com>
2024-02-18 18:12:16 +0700

    add seo

--- src/views/pages/home.tsx
+++ src/views/pages/home.tsx
@@ -26,6 +26,7 @@ const HomePage = ({
     >
       <BaseHead>
         <title safe>{process.env.HOME_PAGE_TITLE}</title>
+        <meta name="description" content={process.env.HOME_PAGE_DESCRIPTION} />
 
         <meta name="twitter:card" content="summary_large_image" />
 
