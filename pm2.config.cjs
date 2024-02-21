4cf2b3ccda397a44fdd8cd279cac714aed2e40c9
nghyane <hoangvananhnghia@gmail.com>
2024-02-21 17:10:52 +0700

    fix index path

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   name: "app", // Name of your application
-  script: "/src/index.ts", // Path to the main script
+  script: "src/index.ts", // Path to the main script
   interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
 };
