eea5056f972bfad9494c19ffa1cc86690fbb7439
nghyane <hoangvananhnghia@gmail.com>
2024-02-26 23:57:26 +0700

    fix

--- src/services/url.ts
+++ src/services/url.ts
@@ -1,19 +1,22 @@
 import slugify from "slugify";
 
 const titleToSlug = (title: string) => {
-  // limit to 100 characters
-  return slugify(title, {
-    locale: "ja",
-    remove: /[*+～ー?.()'"!:@\/]/g,
-    lower: true,
-    replacement: "-",
-  }).slice(0, 150);
+    // Create slug from title
+    let slug = slugify(title, {
+        locale: "ja",
+        remove: /[*+～ー?.()'"!:@\/]/g, // Remove specified characters
+        lower: true,
+        replacement: "-" // Replace removed characters with '-'
+    }).replace(/#/g, ""); // Remove '#' character
+
+    return slug.slice(0, 150);
 };
 
-export const mangaUrl = ({ title, id }: { title: string; id: number }) => {
-  return `/manga/${titleToSlug(title)}/${id}`;
+
+export const mangaUrl = ({title, id}: { title: string; id: number }) => {
+    return `/manga/${titleToSlug(title)}/${id}`;
 };
 
-export const chapterUrl = ({ id }: { id: number }) => {
-  return `/read?id=${id}`;
+export const chapterUrl = ({id}: { id: number }) => {
+    return `/read?id=${id}`;
 };
